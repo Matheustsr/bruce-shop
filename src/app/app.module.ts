da8f968323b464b0fe017ae5ca0dc892fb0e3ed2
unknown <matheustsr@outlook.com>
2021-06-23 06:47:28 -0300

    Added checkout/login/pets pages components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
+import { PetsPageComponent } from './pets-page/pets-page.component';
+import { ProductsPageComponent } from './products-page/products-page.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginPageComponent,
+    ResetPasswordPageComponent,
+    SignupPageComponent,
+    PetsPageComponent,
+    ProductsPageComponent,
+    CartPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/pets-page/pets-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PetsPageComponent } from './pets-page.component';
+
+describe('PetsPageComponent', () => {
+  let component: PetsPageComponent;
+  let fixture: ComponentFixture<PetsPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PetsPageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PetsPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
