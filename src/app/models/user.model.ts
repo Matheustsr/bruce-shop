dcdd97033c3a68be800ae9a32071ae31ff065f7f
Matheus vieira <matheustsr@outlook.com>
2021-07-02 06:58:51 -0300

    user model fixed

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,9 +1,9 @@
 export class User {
   constructor(
-      public _id!: string;
-      public name!: string;
-      public document!: string;
-      public email!: string;
+      public _id: string,
+      public name: string,
+      public document: string,
+      public email: string
   ) {
 
   }
